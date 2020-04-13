1659b5b2d5365cfaf049ff4017fd77f5c1f11ca0
황주빈 <jebi0825@naver.com>
2020-04-13 17:49:25 +0900

    - 황주빈 list.jsp 수정, vo 수정

--- team/src/main/java/com/team/vo/Member.java
+++ team/src/main/java/com/team/vo/Member.java
@@ -14,6 +14,7 @@ public class Member {
 	private String demartment;
 	private String position;
 	private boolean deleted;
+	private String img;
 	
 	private Address address;
 }
---
+++ team/src/main/java/com/team/vo/UserType.java
@@ -0,0 +1,9 @@
+package com.team.vo;
+
+import lombok.Data;
+
+@Data
+public class UserType {
+	private int typeNo;
+	private String type;
+}
