9a4532d84afb03eec695348fceb36ef74b3c832b
황주빈 <jebi0825@naver.com>
2020-05-11 16:46:08 +0900

    홈 매핑

--- team/src/main/java/com/team/controller/HomeController.java
+++ team/src/main/java/com/team/controller/HomeController.java
@@ -11,15 +11,7 @@ public class HomeController {
 
 	@GetMapping({"/", "/home"})
 	public String home(Model model, HttpSession session) {
-		
-		
 		return "/home";
-	}
-	
-	
-	@GetMapping("/home2")
-	public String home2() {
-		return "/home2";
 	}
 	
 }
